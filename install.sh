1b27344d3082c24c7ddd7c8229918f1009eea3c7
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-11 22:05:53 +0200

    add magic install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+REPO="changelog"
+ARCH=$(uname -m)
+case $ARCH in
+	arm*) ARCH="arm";;
+	x86) ARCH="386";;
+	x86_64) ARCH="amd64";;
+esac
+latest=`wget -q --no-check-certificate -O - https://api.github.com/repos/mh-cbon/${REPO}/releases/latest | grep -E '"tag_name": "([^"]+)"' | cut -d '"' -f4`
+wget --no-check-certificate https://github.com/mh-cbon/${REPO}/releases/download/${latest}/go-bin-deb-${ARCH}.deb
+sudo dpkg -i ${REPO}-${ARCH}.deb
