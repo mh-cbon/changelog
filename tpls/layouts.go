a5b07e28f5179e216618668b9e996eafcd7a78b1
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-14 15:29:29 +0200

    tpls: Fix RPM template, Refactor to simplify code

--- tpls/layouts.go
+++ tpls/layouts.go
@@ -4,7 +4,7 @@ var MD = `{{if eq .partial false}}# Changelog - {{.vars.name}}
 {{end}}
 
 {{- range $e := .changelog.Versions}}
-### {{if call $.isnil $e.Version }}{{$e.Name}}{{else}}{{ $e.Version.String }}{{end}}
+### {{$e.GetName}}
 
 __Changes__
 {{range $change := $e.Changes}}
@@ -23,7 +23,7 @@ ______________
 `
 
 var CHANGELOG = `{{- range $index, $e := .changelog.Versions}}
-{{- if call $.isnil $e.Version }}{{$e.Name}}{{else}}{{$e.Version.String}}{{end}}
+{{$e.GetName}}
 {{range $change := $e.Changes}}
   * {{$change}}
 {{- end}}
@@ -31,25 +31,25 @@ var CHANGELOG = `{{- range $index, $e := .changelog.Versions}}
   - {{$contributor}}
 {{- end}}
 
--- {{$e.Author.Name}}{{if gt ($e.Author.Email|len) 0}} <{{$e.Author.Email}}>{{end}}; {{$e.GetDate}}
+-- {{$e.Author.String}}; {{$e.GetDate}}
 
 
 {{end}}`
 
 var DEBIAN = `{{- range $index, $e := .changelog.Versions}}
-{{$.vars.name}} ({{- if call $.isnil $e.Version }}{{$e.Name}}{{else}}{{$e.Version.String}}{{end}})
+{{$.vars.name}} ({{$e.GetName}})
 {{- with $urgency := $e.GetTag "urgency" }}
 {{- if gt ($urgency|len) 0}}{{$urgency}};{{end}}{{- range $k,$v := $e.Tags}}{{$k}}={{$v}};{{- end}}{{end}}
 {{range $change := $e.Changes}}
   * {{$change}}
 {{- end}}
 
--- {{$e.Author.Name}}{{if gt ($e.Author.Email|len) 0}} <{{$e.Author.Email}}>{{end}}  {{$e.GetDateF $.debianlayout}}
+-- {{$e.Author.String}}  {{$e.GetDateF $.debianlayout}}
 
 {{end}}`
 
 var RPM = `{{- range $index, $e := .changelog.Versions}}
-* {{$e.GetDateF $.rpmlayout}} {{$e.Author.String}} - {{if call $.isnil $e.Version }}{{$e.Name}}{{else}}{{$e.Version.String}}-{{$e.GetTag "release"}}{{end}}
+* {{$e.GetDateF $.rpmlayout}} {{$e.Author.String}} - {{$e.GetName}}{{if gt ($e.GetTag "release"|len) 0}}-{{$e.GetTag "release"}}{{end}}
 {{- range $change := $e.Changes}}
 - {{$change}}
 {{- end}}
