57834c7eac2240768ce11dd8c9b877bf45697850
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-14 19:15:32 +0200

    build: add rpm packages

---
+++ docker.sh
@@ -0,0 +1,10 @@
+dnf install rpm-build -y
+wget -q -O - --no-check-certificate \
+https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh \
+| sh -x  mh-cbon/go-bin-rpm '${REPO}-${ARCH}${EXT}'
+cd /docker
+TAG=$1
+NAME=$2
+if [[ -z ${TAG} ]]; then TAG="0.0.0"; fi
+VERBOSE=* ./go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o ${NAME}-386.rpm
+VERBOSE=* ./go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o ${NAME}-amd64.rpm
