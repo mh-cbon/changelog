ea980ad961e8c7ef8cd31c4e6ea9547280949317
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-14 22:02:41 +0200

    travis: fix build script

--- docker.sh
+++ docker.sh
@@ -11,5 +11,5 @@ NAME=$2
 
 if [[ -z ${TAG} ]]; then TAG="0.0.0"; fi
 
-VERBOSE=* ./go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o ${NAME}-386.rpm
-VERBOSE=* ./go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o ${NAME}-amd64.rpm
+VERBOSE=* go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o ${NAME}-386.rpm
+VERBOSE=* go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o ${NAME}-amd64.rpm
