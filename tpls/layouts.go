b3347402fda29c57e96904793d89c42429eae6a6
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-26 18:04:51 +0200

    layouts: add new GHRELEASE template

--- tpls/layouts.go
+++ tpls/layouts.go
@@ -4,11 +4,12 @@ var MD = `{{if eq .partial false}}# Changelog - {{.vars.name}}
 {{end}}
 
 {{- range $e := .changelog.Versions}}
+{{- $tagRange := call $.getTagRange $e.GetName}}
 ### {{$e.GetName}}
 
 __Changes__
 {{range $change := $e.Changes}}
-- {{$change}}
+{{call $.printMultilines $change "- "}}
 {{- end}}
 {{if gt ($e.Contributors | len) 0}}
 __Contributors__
@@ -16,7 +17,8 @@ __Contributors__
 - {{$contributor.Name}}
 {{- end}}
 {{end}}
-Released by {{$e.Author.Name}}, {{$e.Date.Format "Mon 02 Jan 2006"}}
+Released by {{$e.Author.Name}}, {{$e.Date.Format "Mon 02 Jan 2006"}} -
+[see the diff](https://github.com/mh-cbon/changelog/compare/{{$tagRange.Begin}}...{{$tagRange.End}}#diff)
 ______________
 {{end}}
 
@@ -25,7 +27,7 @@ ______________
 var CHANGELOG = `{{- range $index, $e := .changelog.Versions}}
 {{$e.GetName}}
 {{range $change := $e.Changes}}
-  * {{$change}}
+{{call $.printMultilines $change "  * "}}
 {{- end}}
 {{range $contributor := $e.Contributors}}
   - {{$contributor}}
@@ -41,7 +43,7 @@ var DEBIAN = `{{- range $index, $e := .changelog.Versions}}
 {{- with $urgency := $e.GetTag "urgency" }}
 {{- if gt ($urgency|len) 0}}{{$urgency}};{{end}}{{- range $k,$v := $e.Tags}}{{$k}}={{$v}};{{- end}}{{end}}
 {{range $change := $e.Changes}}
-  * {{$change}}
+{{call $.printMultilines $change "  * "}}
 {{- end}}
 
 -- {{$e.Author.String}}  {{$e.GetDateF $.debianlayout}}
@@ -51,6 +53,17 @@ var DEBIAN = `{{- range $index, $e := .changelog.Versions}}
 var RPM = `{{- range $index, $e := .changelog.Versions}}
 * {{$e.GetDateF $.rpmlayout}} {{$e.Author.String}} - {{$e.GetName}}{{if gt ($e.GetTag "release"|len) 0}}-{{$e.GetTag "release"}}{{else}}-1{{end}}
 {{- range $change := $e.Changes}}
-- {{$change}}
+{{call $.printMultilines $change "- "}}
 {{- end}}
 {{end}}`
+
+// this layout is mostly useful to create log for
+// gh release page : https://github.com/mh-cbon/go-repo-utils/releases
+var GHRELEASE = `{{- range $e := .changelog.Versions}}
+{{- range $change := $e.Changes}}
+{{call $.printMultilines $change "- "}}
+{{- end}}
+
+__Contributors__ : {{call $.join $e.Contributors.Names ", "}}
+{{- end}}
+`
