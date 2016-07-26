8712a6c1d91c3b88b206934fc78377d8a13322e6
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-26 18:08:05 +0200

    multilines: improved support of multiles changes to correctly align them vertically with the prefix, refactoring: refactored code and added comments

--- tpls/index.go
+++ tpls/index.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"bytes"
 	"strings"
 	"text/template"
 
@@ -14,16 +15,34 @@ import (
 
 var logger = verbose.Auto()
 
-func GenerateTemplate(clog changelog.Changelog, partial bool, vars map[string]interface{}, src string, out string) error {
+// PrintMultilines prints multiple lines with a given prefix
+// replaced by space for line with index > 0
+// (vertical alignment)
+func PrintMultilines (lines string, prefix string) string {
+  ret := ""
+  for index, line := range strings.Split(lines, "\n") {
+    if index==0 {
+      ret += prefix + line + "\n"
+    } else {
+      ret += strings.Repeat(" ", len(prefix)) + line + "\n"
+    }
+  }
+  return strings.TrimSuffix(ret, "\n")
+}
+
+// WriteTemplateTo writes changelog content
+// to out target using src template file
+func WriteTemplateTo(clog changelog.Changelog, partial bool, vars map[string]interface{}, src string, out string) error {
 	d, err := ioutil.ReadFile(src)
 	if err != nil {
 		return err
 	}
-	return GenerateTemplateStr(clog, partial, vars, string(d), out)
+	return WriteTemplateStrTo(clog, partial, vars, string(d), out)
 }
 
-func GenerateTemplateStr(clog changelog.Changelog, partial bool, vars map[string]interface{}, tplString string, out string) error {
-
+// WriteTemplateStrTo write changelog content
+// to out target using given template string
+func WriteTemplateStrTo(clog changelog.Changelog, partial bool, vars map[string]interface{}, tplString string, out string) error {
 	var err error
 	var writer io.Writer
 	if out == "-" {
@@ -37,20 +56,51 @@ func GenerateTemplateStr(clog changelog.Changelog, partial bool, vars map[string
 		writer = f
 	}
 
+  content, err := GenerateTemplateStr(clog, partial, vars, tplString)
+  if err!=nil {
+    return err
+  }
+
+  _, err = writer.Write([]byte(content))
+	return err
+}
+
+// GenerateTemplate generates the changelog content
+// using given src template file
+func GenerateTemplate(clog changelog.Changelog, partial bool, vars map[string]interface{}, src string) (string, error) {
+	tplString, err := ioutil.ReadFile(src)
+	if err != nil {
+		return "", err
+	}
+	return GenerateTemplateStr(clog, partial, vars, string(tplString))
+}
+
+// GenerateTemplateStr generates changelog content
+// using given template string
+func GenerateTemplateStr(clog changelog.Changelog, partial bool, vars map[string]interface{}, tplString string) (string, error) {
+
 	values := make(map[string]interface{})
 	values["changelog"] = clog
+	values["getTagRange"] = clog.GetTagRange
 	values["partial"] = partial
 	values["vars"] = vars
 	values["isnil"] = IsNil
 	values["debianlayout"] = changelog.DateLayouts[0]
 	values["rpmlayout"] = changelog.DateLayouts[4]
 	values["join"] = strings.Join
+	values["printMultilines"] = PrintMultilines
 
 	t, err := template.New("it").Parse(tplString)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return t.Execute(writer, values)
+
+  var b bytes.Buffer
+	err = t.Execute(&b, values)
+  if err !=nil {
+    return "", err
+  }
+  return b.String(), nil
 }
 
 func IsNil(args *semver.Version) bool {
